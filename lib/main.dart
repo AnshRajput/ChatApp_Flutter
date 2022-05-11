8be7443091e91a5756f86d8520b56a9508bec370
AnshRajput <63461718+AnshRajput@users.noreply.github.com>
2022-05-11 13:04:02 +0530

    Add files via upload
    Initial Commmit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chatapplication/services/auth.dart';
+import 'package:chatapplication/views/home.dart';
+import 'package:chatapplication/views/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        future: AuthMethods().getCurrentUser(),
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          } else {
+            return SignIn();
+          }
+        },
+      ),
+    );
+  }
+}
